fa4f51086b6f6853774d027102fc82e432ac464f
yellowssun <hbsun98@gmail.com>
2022-08-13 11:43:14 +0900

    Revert "Revert "feat: add new dataset(Robustscaler)""
    This reverts commit 3a7d3929563410301fec0eb6d7bf961cd66bc444.

--- Byungsun_main/create_model_LSTM/create_test_body.py
+++ Byungsun_main/create_model_LSTM/create_test_body.py
@@ -2,27 +2,30 @@ import cv2
 import numpy as np
 import time
 import os
-from operation import get_points_angle
-from sklearn.preprocessing import Normalizer
-from draw_function import custom_landmarks, set_cam
+from operation import get_points_angle_13, get_points_angle_17
+from sklearn.preprocessing import Normalizer, RobustScaler
+from draw_function import custom_landmarks_13, custom_landmarks_17 , set_cam
 
 leg_raise = ['leg_raise_F', 'leg_raise_T']
 knee_up = ['knee_up_F', 'knee_up_T']
 side_crunch = ['side_crunch_F', 'side_crunch_T']
 side_lunge = ['side_lunge_F', 'side_lunge_T']
 
-actions = [side_lunge]
-_actions = ['side_lunge']
+
+actions = [leg_raise, knee_up, side_crunch, side_lunge]
+_actions = ['leg_raise', 'knee_up', 'side_crunch', 'side_lunge']
+
 
 N_scaler = Normalizer()
+R_scaler = RobustScaler()
 pose_angles = ['C']
 created_time = int(time.time()) 
 sequence_length = 15
 pose = set_cam()
-os.makedirs('C:/Users/UCL7/VS_kwix/test_dataset_v4', exist_ok=True)
+os.makedirs('C:/Users/UCL7/VS_kwix/test_dataset_v5', exist_ok=True)
 
 for i, _ in enumerate(actions):
-    os.makedirs('C:/Users/UCL7/VS_kwix/test_dataset_v4/' + _actions[i], exist_ok=True)
+    os.makedirs('C:/Users/UCL7/VS_kwix/test_dataset_v5/' + _actions[i], exist_ok=True)
     path_dir = 'E:/' + _actions[i]
     for idx, action in enumerate(_):
         start = time.time()
@@ -52,11 +55,11 @@ for i, _ in enumerate(actions):
                 img = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2BGR)
 
                 if results.pose_landmarks is not None:
-                    landmark_subset = custom_landmarks(results)
-                    joint, angle = get_points_angle(landmark_subset)
+                    landmark_subset = custom_landmarks_17(results)
+                    joint, angle = get_points_angle_17(landmark_subset)
 
                     reshape_angle = np.degrees(angle).reshape(-1, 1)
-                    scaled_angle = N_scaler.fit_transform(reshape_angle)
+                    scaled_angle = R_scaler.fit_transform(reshape_angle)
 
                     angle_label = np.array(
                         [scaled_angle], dtype=np.float32)
@@ -68,7 +71,7 @@ for i, _ in enumerate(actions):
 
         data = np.array(data)
         print(action, data.shape)
-        np.save(os.path.join('C:/Users/UCL7/VS_kwix/test_dataset_v4/' + _actions[i],
+        np.save(os.path.join('C:/Users/UCL7/VS_kwix/test_dataset_v5/' + _actions[i],
                 f'raw_{action}_{created_time}'), data)
 
         full_seq_data = []
@@ -77,6 +80,6 @@ for i, _ in enumerate(actions):
 
         full_seq_data = np.array(full_seq_data)
         print(action, full_seq_data.shape)
-        np.save(os.path.join('C:/Users/UCL7/VS_kwix/test_dataset_v4/' + _actions[i],
+        np.save(os.path.join('C:/Users/UCL7/VS_kwix/test_dataset_v5/' + _actions[i],
                 f'seq_{action}_{created_time}'), full_seq_data)
         print("Working time : ", time.time() - start)
